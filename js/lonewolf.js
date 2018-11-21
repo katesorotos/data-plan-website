0e5eb39c986374ee603a89bddebd88ffa48f3823
Kate Egle Sorotos <kate_sorotos@live.co.uk>
2018-11-21 11:30:19 +0000

    changes lonewolf calculator

--- js/lonewolf.js
+++ js/lonewolf.js
@@ -5,14 +5,20 @@ var choices = ["3GB", "6GB", "15GB", "30GB"];
 var sub_total = 0;
 var total = 0;
 
+$(".hide_total").hide();
+$("#payment").hide();
 
 $(".wolf--price").click(function(event) {
+   sub_total = 0;
    var wolf_gb = this.getAttribute("data-gb");
-  //
+
    var wolf_price = parseInt(this.getAttribute("data_price"));
    sub_total = sub_total + wolf_price;
    $("#sub--total").text(sub_total);
    console.log(sub_total);
+   $("#payment").show();
+   $(".hide_sub").show();
+   $(".hide_total").hide();
 });
 
 $(".wolf--plan").click(function(event) {
@@ -20,4 +26,5 @@ $(".wolf--plan").click(function(event) {
   total = sub_total * wolf_month;
   $("#total").text(total);
   console.log(total);
+  $(".hide_total").show();
 })
